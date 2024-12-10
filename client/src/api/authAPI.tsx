dd999e1fcfc695556286a0c5551b0d1bd24d75d5
DDeMarois <Drew.DeMarois@du.edu>
2024-12-10 15:37:26 -0700

    Implement user login API call with error handling

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,25 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+    if (!response.ok) {
+      const errordata = await response.json();
+      throw new Error(`Error ${errordata.message}`);
+    }
+  
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error from user login: ', error);
+    return Promise.reject('Could not fetch user info');
+  }
 }
-
-
 
 export { login };
